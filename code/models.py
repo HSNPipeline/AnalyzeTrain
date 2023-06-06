5cca50e6429dd78edc6f0b907914184f4c90d3c5
weijiazh97 <wz2540@columbia.edu>
2023-06-06 18:09:15 -0400

    filled in missing code function

--- code/models.py
+++ code/models.py
@@ -6,3 +6,11 @@ from spiketools.stats.anova import create_dataframe, create_dataframe_bins, fit_
 
 ###################################################################################################
 ###################################################################################################
+PLACE = {
+    'MODEL' : 'fr ~ C(bin)',
+    'FEATURE' : 'C(bin)',
+    'COLUMNS' : ['bin', 'fr']
+}
+
+create_df_place = partial(create_dataframe_bins)
+fit_anova_place = partial(fit_anova, formula=PLACE['MODEL'], feature=PLACE['FEATURE'])
--- code/reports.py
+++ code/reports.py
@@ -2,3 +2,44 @@
 
 ###################################################################################################
 ###################################################################################################
+
+import numpy as np
+
+from spiketools.measures.spikes import compute_firing_rate, compute_presence_ratio
+
+from spiketools.utils.base import count_elements
+from spiketools.utils.extract import drop_range
+from spiketools.utils.timestamps import convert_sec_to_min
+
+
+
+def create_unit_info(unit, uind):
+    """Create a dictionary of unit information."""
+
+    spikes = unit.spike_times.data[:]
+    unit_info = {}
+    unit_info['n_spikes'] = len(spikes)
+    unit_info['firing_rate'] = float(compute_firing_rate(spikes))
+    unit_info['presence_ratio'] = float(compute_presence_ratio(spikes, 40.0))
+    unit_info['first_spike'] = spikes[0]
+    unit_info['last_spike'] = spikes[-1]
+
+    return unit_info
+
+
+def create_unit_str(unit_info):
+    
+    string = '\n'.join([
+        '\n',
+        '\n',
+        'spikes:   {:5d}'.format(unit_info['n_spikes']),
+        'firing rate:  {:5.4f}'.format(unit_info['firing_rate']),
+        'prence_ratio: {:5.4f}'.format(unit_info['presence_ratio']),
+        'first_spike: {:5.4f}'.format(unit_info['first_spike']),
+        'last_spike: {:5.4f}'.format(unit_info['last_spike']),
+          
+       ])
+    return string
+
+
+
